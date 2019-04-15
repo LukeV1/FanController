04d91565a873207efa8497832f0ef4fce8ce59bc
Luka Treftz <st148498@stud.uni-stuttgart.de>
2019-04-15 13:21:55 +0200

    Update examples - include needed library - increase delay to reduce unnecessary output

--- examples/Monitor/Monitor.ino
+++ examples/Monitor/Monitor.ino
@@ -1,10 +1,11 @@
-// Include the library
+// Include the libraries (order matters!)
+#include <EnableInterrupt.h>
 #include <FanController.h>
 
-// Sensor wire is plugged into port 2 on the Arduino.
+// Sensor wire is plugged into pin 8 on the Arduino.
 // For a list of available pins on your board,
-// please refer to: https://www.arduino.cc/en/Reference/AttachInterrupt
-#define SENSOR_PIN 2
+// please refer to: https://github.com/GreyGnome/EnableInterrupt/wiki/Usage
+#define SENSOR_PIN 8
 
 // Choose a threshold in milliseconds between readings.
 // A smaller value will give more updated results,
@@ -39,6 +40,6 @@ void loop(void)
   Serial.println("RPM");
 
   // Not really needed, just avoiding spamming the monitor,
-  // readings will be performed no faster than once every THRESHOLD ms anyway
-  delay(250);
+  // readings will be performed not faster than once every THRESHOLD ms anyway
+  delay(500);
 }
